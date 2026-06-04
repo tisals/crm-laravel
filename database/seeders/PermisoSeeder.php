66bcbcdf3387122d637beb452c9036b12feab661
Tisals <innovacionydesarrollo.tis@gmail.com>
2026-06-04 17:10:15 -0500

    fix(seeder): assign wildcard to SuperAdmin and define all CRM/Admin views in PermisoSeeder

--- database/seeders/PermisoSeeder.php
+++ database/seeders/PermisoSeeder.php
@@ -10,10 +10,9 @@ class PermisoSeeder extends Seeder
 {
     public function run(): void
     {
-        $admin = Rol::where('nombre', 'Admin')->first();
+        $superAdmins = Rol::whereIn('nombre', ['SuperAdmin', 'Admin'])->get();
 
-        if ($admin) {
-            // Admin gets wildcard permission
+        foreach ($superAdmins as $admin) {
             Permiso::firstOrCreate([
                 'rol_id' => $admin->id,
                 'vista' => '*',
@@ -27,9 +26,21 @@ class PermisoSeeder extends Seeder
             'ciudades' => ['index', 'show'],
             'productos' => ['index', 'store', 'show', 'update', 'destroy'],
             'etiquetas' => ['index', 'store', 'show', 'update', 'destroy'],
+            'entidad' => ['index', 'store', 'show', 'update', 'destroy'],
+            'contacto' => ['index', 'store', 'show', 'update', 'destroy', 'acciones'],
+            'oportunidades' => ['index', 'store', 'show', 'update', 'destroy', 'ganar', 'clonar', 'version'],
+            'seguimientos' => ['index', 'store', 'show', 'update', 'destroy'],
+            'maestros' => ['index', 'store', 'show', 'update', 'destroy'],
+            'servicios' => ['index', 'store', 'show', 'update', 'destroy', 'renew', 'byEntidad'],
+            'detalles-servicio' => ['index', 'store', 'show', 'update', 'destroy'],
+            'ordenes-servicio' => ['index', 'store', 'show', 'update', 'destroy'],
+            'cuentas' => ['index', 'store', 'show', 'update', 'destroy'],
+            'movimientos' => ['index', 'store', 'show', 'update', 'destroy'],
+            'entidad-usuario' => ['index', 'store', 'destroy'],
+            'pipelines' => ['index'],
         ];
 
-        $roles = Rol::where('nombre', '!=', 'Admin')->get();
+        $roles = Rol::whereNotIn('nombre', ['SuperAdmin', 'Admin'])->get();
 
         foreach ($roles as $rol) {
             foreach ($vistas as $vista => $acciones) {
